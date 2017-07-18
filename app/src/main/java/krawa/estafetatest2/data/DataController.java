539155c0b6e5a3e91c8bb91d43dd4c549bacc047
krawa <kraw4enko@bigmir.net>
2017-07-18 10:41:48 +0300

    request current location

--- app/src/main/java/krawa/estafetatest2/data/DataController.java
+++ app/src/main/java/krawa/estafetatest2/data/DataController.java
@@ -1,22 +1,59 @@
 package krawa.estafetatest2.data;
 
+import android.location.Location;
+import android.util.Log;
+
+import java.util.List;
+
 import javax.inject.Inject;
 
 import krawa.estafetatest2.App;
+import krawa.estafetatest2.model.Image;
 import krawa.estafetatest2.model.SearchResult;
+import krawa.estafetatest2.utils.LocationUtils;
 
 public class DataController implements DataSource{
 
+    private static final String TAG = "DataController";
+
     @Inject
     NetworkDataSource networkDataSource;
+    @Inject
+    LocationUtils locationUtils;
 
     public DataController(){
         App.getAppComponent().inject(this);
     }
 
     @Override
-    public void searchImages(String query, DataListener<SearchResult> listener) {
-        networkDataSource.searchImages(query, listener);
+    public void searchImages(String query, final DataListener<SearchResult> listener) {
+        networkDataSource.searchImages(query, new DataListener<SearchResult>() {
+            @Override
+            public void onResult(SearchResult result) {
+                listener.onResult(result);
+                getLocationAndSaveImages(result.getItems());
+            }
+
+            @Override
+            public void onFailure(String error) {
+                listener.onFailure(error);
+            }
+        });
+    }
+
+    private void getLocationAndSaveImages(final List<Image> items) {
+        Log.d(TAG, "getLocationAndSaveImages");
+        locationUtils.getCurrentLocation(new LocationUtils.OnCurrentLocationCallback() {
+            @Override
+            public void onCurrentLocationResult(Location location) {
+                Log.d(TAG, "getLocationAndSaveImages location="+location);
+                saveImages(items, location);
+            }
+        });
+    }
+
+    private void saveImages(List<Image> images, Location location) {
+
     }
 
 }
--- app/src/main/java/krawa/estafetatest2/utils/LocationUtils.java
+++ app/src/main/java/krawa/estafetatest2/utils/LocationUtils.java
@@ -1,12 +1,50 @@
 package krawa.estafetatest2.utils;
 
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 
 public class LocationUtils {
 
-    public LocationUtils(Context context) {
+    private Context mContext;
+    private GoogleApiClient mGoogleApiClient;
 
+    public LocationUtils(Context context) {
+        mContext = context;
+        mGoogleApiClient = new GoogleApiClient.Builder(context)
+                .addApi(LocationServices.API)
+                .build();
+        mGoogleApiClient.connect();
+    }
+
+    public void getCurrentLocation(final OnCurrentLocationCallback callback) {
+        LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setNumUpdates(1);
+        mLocationRequest.setExpirationTime(60 * 1000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        if (ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            callback.onCurrentLocationResult(null);
+            return;
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                callback.onCurrentLocationResult(location);
+            }
+        });
+    }
+
+    public interface OnCurrentLocationCallback {
+        void onCurrentLocationResult(Location location);
     }
 
 }
